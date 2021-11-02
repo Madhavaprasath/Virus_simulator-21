f6c1bb8fe473102826b59d05a5410dc6dba4fdc2
João Dowsley <joao.dowsley850@gmail.com>
2021-11-02 19:41:12 -0300

    Add virus mechanics

--- Project/Computer/Computer.gd
+++ Project/Computer/Computer.gd
@@ -3,10 +3,26 @@ extends Control
 onready var task_manager=get_node("TextureRect/Startmenu")
 onready var tutorial=get_node("TextureRect/Tutorial")
 onready var trash=get_node("TextureRect/Trash")
+onready var hill=get_node("TextureRect/HillCollision")
+
+var allow_new_window := true # To avoid 2 simultaneous windows
 
 func _ready():
 	for button in get_tree().get_nodes_in_group("Icons"):
 		button.connect("pressed",self,"on_icons_pressed",[button.get_groups(),button.name])
+
+func toggle_collisions(body, state=null):
+	if not body:
+		return
+	# Every child should be a CollisionShape
+	# Default: Switches value
+	for c in body.get_children():
+		if state != null:
+			c.disabled = not state
+		else:
+			c.disabled = not c.disabled
+		print(body.get_parent().name)
+		print(c.disabled)
 
 func on_icons_pressed(group,b_name):
 	match group[1]:
@@ -16,18 +32,33 @@ func on_icons_pressed(group,b_name):
 		"Internet":
 			OS.shell_open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 		"Start":
-			task_manager.visible=not task_manager.visible
+			task_manager.visible = not task_manager.visible
 		"Shutdown":
 			get_tree().quit()
 		"Restart":
 			get_tree().reload_current_scene()
 		"Tutorial":
-			tutorial.visible=true
+			if allow_new_window:
+				tutorial.visible=true
+				allow_new_window=false
+				toggle_collisions(tutorial.get_node("StaticBody2D"))
+				toggle_collisions(hill)
 		"Trash":
-			trash.visible=true
+			if allow_new_window:
+				trash.visible=true
+				allow_new_window=false
+				toggle_collisions(trash.get_node("StaticBody2D"))
+				toggle_collisions(hill)
 		"Close":
 			var window="TextureRect/"+str(b_name)
-			get_node(window).visible=false
+			get_node(window).visible = false
+			allow_new_window = true
+			print("TOGGLE COLLISION")
+			toggle_collisions(
+				get_node(window).get_node("StaticBody2D"),
+				false
+			)
+			toggle_collisions(hill, true)
 
 func open_scene(area_name):
 	return ("res://Platformer/Scenes/"+str(area_name)+"/"+str(area_name)+"-level1.tscn")
